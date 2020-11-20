bd21cd92a2d973ce5d544725a6d13d1cafcb8d69
hymntaha <tahauygun@gmail.com>
2020-11-19 23:04:19 -0800

    adding product comp

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBp18O6ia0n1PWdXDc0yBZU4MuPSaEbaMo",
+  authDomain: "clone-app-9d43d.firebaseapp.com",
+  databaseURL: "https://clone-app-9d43d.firebaseio.com",
+  projectId: "clone-app-9d43d",
+  storageBucket: "clone-app-9d43d.appspot.com",
+  messagingSenderId: "117811991920",
+  appId: "1:117811991920:web:1db95b66f3f3ec70746bb5",
+  measurementId: "G-B7QL21NM3W",
+};
